51192d406c9d6e1063349f57d825dbd8a6cf2d23
amarildolikmeta <amarildolikmeta@gmail.com>
2024-05-29 11:12:15 +0200

    minor fix

--- run_servers_and_environment.sh
+++ run_servers_and_environment.sh
@@ -5,7 +5,7 @@ scp=ai_optimizer/configs/simulator_config_"$n_agents"units.json
 pcp=ai_optimizer/configs/simulator_config_"$n_agents"units.json
 lcp=ai_optimizer/configs/learning_config.json
 
-python3 run_environment &
+python3 run_environment.py &
 for ((i=0;i<$n_agents;i+=1))
   do
       python3 ai_optimizer/server_creator.py --addr localhost --port $((base_port + i)) --scp $scp --product_config_path $pcp --lcp $lcp --workers 0 --cppu cppu_$i --algo SAC --checkpoint 0 --outdir output/ --seed $i &
